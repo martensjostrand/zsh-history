f8787d8a23125da1f2ccdeb015032b26c7e788e2
Mårten Sjöstrand <marten.sjostrand@gmail.com>
2019-10-16 22:31:51 +0200

    Add default history conf

---
+++ zsh-history.plugin.zsh
@@ -0,0 +1,6 @@
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=10000
+setopt    appendhistory     #Append history to the history file (no overwriting)
+setopt    sharehistory      #Share history across terminals
+setopt    incappendhistory
